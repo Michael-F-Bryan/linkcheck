24a21977f848555f87a9fd3cfc6ef172dac46487
Michael-F-Bryan <michaelfbryan@gmail.com>
2020-04-24 02:22:00 +0800

    Removed the links stuff

--- src/scanners/mod.rs
+++ src/scanners/mod.rs
@@ -10,26 +10,3 @@ mod plaintext;
 pub use markdown::{markdown, markdown_with_broken_link_callback};
 #[cfg(feature = "plaintext")]
 pub use plaintext::plaintext;
-
-use codespan::{FileId, Span};
-
-#[derive(Debug, Clone, PartialEq)]
-#[non_exhaustive]
-pub struct Link {
-    pub href: String,
-    pub span: Span,
-    pub file: FileId,
-}
-
-/// Use a *scanner* to extract all the links from some text.
-pub fn links<S, I>(
-    src: &str,
-    file: FileId,
-    scanner: S,
-) -> impl Iterator<Item = Link>
-where
-    S: FnOnce(&str) -> I,
-    I: Iterator<Item = (String, Span)>,
-{
-    scanner(src).map(move |(href, span)| Link { href, span, file })
-}
