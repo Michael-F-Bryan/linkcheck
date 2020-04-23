1f46c6577161dbeb7dc340423fe7597c73a029c4
Michael-F-Bryan <michaelfbryan@gmail.com>
2020-04-24 02:47:23 +0800

    Removed the feature flags

--- src/scanners/mod.rs
+++ src/scanners/mod.rs
@@ -1,12 +1,8 @@
 //! A *scanner* is just a function that which can extract links from a body of
 //! text.
 
-#[cfg(feature = "markdown")]
 mod markdown;
-#[cfg(feature = "plaintext")]
 mod plaintext;
 
-#[cfg(feature = "markdown")]
 pub use markdown::{markdown, markdown_with_broken_link_callback};
-#[cfg(feature = "plaintext")]
 pub use plaintext::plaintext;
